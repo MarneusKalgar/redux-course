70705305536d0df534f16cb9c3ee109405338a24
Igor Chernobay <marneus.kalgar90@gmail.com>
2018-09-17 23:45:20 +0300

    set basic store

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { connect } from 'react-redux'
 import './App.css'
-
-const Test = () => <p>render</p>
 
 class App extends Component {
   render() {
+    const { name, surname, age } = this.props.user
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro">Здесь будут мои самые залайканые фото</p>
+        <p>
+          Привет из App, {name} {surname}!
         </p>
+        <p>Тебе уже {age} ?</p>
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = store => {
+  console.log(store)
+  return {
+    user: store.user,
+  }
+}
+
+export default connect(mapStateToProps)(App)
--- testapp/src/index.js
+++ testapp/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/configureStore'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
